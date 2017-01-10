01d7a15555271f469fb7bcb607d1b00bb12b7990
Rubiksron <rubiksron@gmail.com>
2017-01-10 15:34:32 -0800

    added three tests; one to test text that is POSTED, and two tests to check 200 and 400 status responses, they all pass.

--- lab-ron/test/server-test.js
+++ lab-ron/test/server-test.js
@@ -1,14 +1,32 @@
 'use strict';
 const request = require('superagent');
 const expect = require('chai').expect;
-const assert = require('assert');
-// const chaiHttp  = require('chai-http');
-
 
 describe('cowsay response', function() {
-  describe('what you should see when you c.u.r.l. http://localhost:3000/cowsay?text=HolyCow!', function() {
+  describe('POST request, what you should see when you c.u.r.l. http://localhost:3000/cowsay?text=HolyCow!', function(done) {
     it('should read HolyCow!', function() {
-      assert.equal()
-    })
-  })
-})
+      request.post('http://localhost:3000/cowsay?text=HolyCow!')
+      .end((err, res) => {
+        expect(res.text).to.equal('HolyCow!');
+        done();
+      });
+    });
+  });
+});
+
+describe('GET request for Ron\'s Cowsay server', function() {
+  it('should return a 200 status code when a valid GET request is made', function(done) {
+    request.get('http://localhost:3000/cowsay?text=HolyCow')
+     .end((err, res) => {
+       expect(res.status).to.equal(200);
+       done();
+     });
+  });
+  it('should return a 400 status code when an invalid GET request is made', function(done) {
+    request.get('http://localhost:3000/cowsay?invalidrequest=brokenYouGet400')
+     .end((err, res) => {
+       expect(res.status).to.equal(400);
+       done();
+     });
+  });
+});
