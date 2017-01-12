38d05bd972982bcf0eed3d7bfcb2d703e0fae073
Michael Bishop <polishthebishop@gmail.com>
2017-01-11 17:45:17 -0800

    started tests

---
+++ lab-michael/test/cowsay-test.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const expect = require('chai').expect;
+const request = require('superagent');
+const cowsay = require('cowsay');
+
+require('../server.js');
+
+describe('Testing The Cowsay', function() {
+  describe('/GET Route', function() {
+    it('will display Hello World at / ', function(done) {
+      request.get('localhost:3000')
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.text).to.equal('Hello World\n');
+        done();
+      });
+    });
+  });
+});
