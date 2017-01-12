d77714783cd3ad0e9b991c03ef95366c192bd349
Michael Bishop <polishthebishop@gmail.com>
2017-01-11 21:01:22 -0800

    POST test

--- lab-michael/test/cowsay-test.js
+++ lab-michael/test/cowsay-test.js
@@ -24,5 +24,38 @@ describe('Testing The Cowsay', function() {
         done();
       });
     });
+    it('will have Error 400 dragon when syntax is incorrect', function(done) {
+      request.get('localhost:3000/cowsay')
+      .end((err, res) => {
+        expect(res.status).to.equal(400);
+        expect(res.text).to.equal(cowsay.say({
+          text: 'Error 400\ntry: localhost:3000/cowsay?text=howdy\n',
+          f:'dragon',
+        }));
+        done();
+      });
+    });
+    it('will have Error 404 Ghostbusters when using wrong route', function(done) {
+      request.get('localhost:3000/co')
+      .end((err, res) => {
+        expect(res.status).to.equal(404);
+        expect(res.text).to.equal(cowsay.say({
+          text: 'ERROR 404!!  \nplease try again.',
+          f: 'ghostbusters',
+        }));
+      });
+      done();
+    });
+  });
+  describe('/POST Route', function() {
+    it('should post a cow with saying Eat-Mor-Chikin', function(done) {
+      request.post('localhost:3000/cowsay')
+      .send({text: 'Eat-Mor-Chikin'})
+      .end((err, res)=> {
+        expect(res.status).to.equal(200);
+        expect(res.text).to.equal(cowsay.say({text: 'Eat-Mor-Chikin'}));
+        done();
+      });
+    });
   });
 });
